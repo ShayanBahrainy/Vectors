e50078f4ce32e0dbcd54e8c1147702e17cfe0908
Shayan B. <64701891+ShayanBahrainy@users.noreply.github.com>
2025-10-27 20:40:55 -0700

    Create two types of lines; Inert and Regular, InertLine is to be used in renderparts, and so does not interface with the renderer itself.

--- engine/line.js
+++ engine/line.js
@@ -1,8 +1,39 @@
-export const Line = class {
-    /**@param {Point} start */
-    /**@param {Point}  end*/
-    /**@param {Number} width*/
+import { Renderer } from "./renderer.js"
+
+export const LineInert = class {
+    /**@param {Point} start
+     * @param {Point}  end
+     * @param {Number} width
+    */
     constructor(start, end, width) {
+        this.x = start.x
+        this.y = start.y
+        this.end = end
+        this.width = width
+        this.shape = "line"
+        this.width = width
+        this.priority = 0
+        this.fillStyle = "rgb(255, 255, 255)"
+    }
+
+    update() {
+
+    }
+
+    collision () {
+        
+    }
+}
+
+export const Line = class {
+    /**
+     * @param {Point} start
+     * @param {Point}  end
+     * @param {Number} width
+     * @param {Renderer} renderer
+    */
+    constructor(start, end, width, renderer) {
+        renderer.addObject(this)
         this.x = start.x
         this.y = start.y
         this.end = end
